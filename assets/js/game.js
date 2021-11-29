17807f24ce78d138496ad5c047c6a1b7e9eac0ea
Mona <you@example.com>
2021-11-28 23:51:43 -0500

    more js edits

--- assets/js/game.js
+++ assets/js/game.js
@@ -60,3 +60,38 @@ startGame = () => {
     getNewQuestion()
 }
 
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore'), score
+
+        return window.location.assign('/end.html')
+    }
+
+    questionCounter++
+    progressText.innerText = 'Question ${questionCounter} of ${MAX_QUESTIONS}'
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+}
+
+choices.forEach(choice => {
+    const number  = choice.dataset['number']
+    choice.innerText = currentQuestion['choice + number']
+})
+
+availableQuestions.splice(questionsIndex, 1)
+
+acceptingAnswers = true
+
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+    if(!acceptingAnswers) return
+
+    acceptingAnswers = false
+    const selectedChoice = e.target
+    const selectedAnswer = selectedChoice.dataset['number']
+
+    
